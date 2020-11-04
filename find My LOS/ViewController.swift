91b69b8533f16c3a5ada7311e21495710df80a18
Arie Satria <satria.arie@gmail.com>
2020-11-04 22:43:40 +0700

    Initial Commit

---
+++ find My LOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  find My LOS
+//
+//  Created by Arie Satria on 04/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ find My LOSTests/find_My_LOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  find_My_LOSTests.swift
+//  find My LOSTests
+//
+//  Created by Arie Satria on 04/11/20.
+//
+
+import XCTest
+@testable import find_My_LOS
+
+class find_My_LOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
